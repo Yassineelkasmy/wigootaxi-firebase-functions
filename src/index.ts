6be0d0d77d6b1a9be3636d306a1ac20e6fac52cc
yassineelkasmy <yassineelkasmy@gmail.com>
2022-08-16 20:57:16 +0100

    add driver stats functions

--- src/index.ts
+++ src/index.ts
@@ -9,8 +9,15 @@ export const ringNearbyDrivers = functions.firestore
   .document("booking/{id}")
   .onCreate(async (snap, context) => {
     const docData = snap.data();
+    const metricsDoc = await admin
+      .firestore()
+      .collection("metrics")
+      .doc("metrics")
+      .get();
+    const metricsData = metricsDoc.data();
+
     // var chosenDriver = "";
-    if (docData != null) {
+    if (docData != null && metricsData) {
       const candidatesUids: string[] = docData.candidatesUids;
       const dest_name = docData.dest_name;
       const start_name = docData.start_name;
@@ -38,6 +45,7 @@ export const ringNearbyDrivers = functions.firestore
           const driverData = driverDoc.data();
           if (driverData != undefined) {
             await driverDoc.ref.update({
+              totalRides: FieldValue.increment(1),
               booking_call: snap.id,
               bookingUserUid: userDoc.id,
               booking: {
@@ -113,6 +121,7 @@ export const ringNearbyDrivers = functions.firestore
                 disttext: docData.disttext,
                 duration: docData.duration,
                 durtext: docData.durtext,
+                price_per_km: metricsData.price_per_km,
                 driverPickedAt: admin.firestore.FieldValue.serverTimestamp(),
                 path: [],
                 pathToStart: [],
@@ -123,7 +132,7 @@ export const ringNearbyDrivers = functions.firestore
               .firestore()
               .collection("drivers")
               .doc(driverDoc.id)
-              .update({ ignored: FieldValue.increment(1) });
+              .update({ ridesIgnored: FieldValue.increment(1) });
           }
         }
         //If we reached here that means no driver has accepted the ride so we must cancell it
@@ -189,6 +198,64 @@ export const saveRides = functions.firestore
         .collection("rides")
         .doc(snap.after.id)
         .set(docData);
+      if (docData.finished) {
+        const totalPrice: number = docData.totalPrice;
+        const metricsDoc = await admin
+          .firestore()
+          .collection("metrics")
+          .doc("metrics")
+          .get();
+        const metricsData = metricsDoc.data()!;
+        const tvaPercentage: number = metricsData.tvaPercentage;
+        const revenuePercentage: number = metricsData.revenuePercentage;
+
+        const tva = totalPrice * tvaPercentage;
+        const revenue = totalPrice * revenuePercentage;
+        const driverRevenue = totalPrice - tva - revenue;
+
+        admin
+          .firestore()
+          .collection("drivers")
+          .doc(driverUid)
+          .collection("rides")
+          .doc(snap.after.id)
+          .update({
+            tva: tva,
+            revenue: revenue,
+            driverRevenue: driverRevenue,
+          });
+
+        admin
+          .firestore()
+          .collection("drivers")
+          .doc(driverUid)
+          .update({
+            totalAmount: FieldValue.increment(totalPrice),
+            revenueToPay: FieldValue.increment(revenue),
+            tvaToPay: FieldValue.increment(tva),
+            driverRevenue: FieldValue.increment(driverRevenue),
+            ridesFinished: FieldValue.increment(1),
+          });
+      }
+      if (docData.cancelledByDriver) {
+        admin
+          .firestore()
+          .collection("drivers")
+          .doc(driverUid)
+          .update({
+            ridesCancelledByDriver: FieldValue.increment(1),
+          });
+      }
+
+      if (docData.cancelledByUser) {
+        admin
+          .firestore()
+          .collection("drivers")
+          .doc(driverUid)
+          .update({
+            ridesCancelledByUser: FieldValue.increment(1),
+          });
+      }
     }
   });
 
